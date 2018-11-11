ddf4a05d502c788ef244ac7ef2d2fa0b1b35e5f1
John <jasiek.ole@gmail.com>
2018-11-11 15:41:58 +0100

    Zadanie 1
    Dodalem dwa komponenty success-alert, warning-alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleBarComponent,
-    CarListComponent
+    CarListComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
