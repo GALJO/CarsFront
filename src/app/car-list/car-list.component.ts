facd2d18dbeade00753441961df8ae9529762491
John <jasiek.ole@gmail.com>
2018-12-05 21:05:59 +0100

    Dodanie id do Car

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../model/car.model';
+import { Guid } from '../utils/guid';
 
 @Component({
   selector: 'app-car-list',
@@ -9,19 +10,20 @@ import { Car } from '../model/car.model';
 export class CarListComponent implements OnInit {
 
   carList: Car[] = [];
-  newCar = new Car(null, null, null, null, null, null);
+  newCar = new Car(null, null, null, null, null, null, null);
 
   constructor() {
     // tslint:disable:max-line-length
-    this.carList.push( new Car('Volkswagen', 'Passat B1', '1973-1977', 45, 'Giorgetto Giugiaro', 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/volkswagen-passat-b1.jpg?alt=media&token=934750e8-b1c8-4f6b-87d2-23ea76328865'));
-    this.carList.push( new Car('Volkswagen', 'Passat B8', 'od 2014', 70, null, 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/volkswagen-passaatB8.jpg?alt=media&token=de2ad730-0b55-4b91-9be1-29fa8703c34e'));
-    this.carList.push( new Car('Toyota', 'Corolla Verso II', '2004-2009', 55, null, 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/toyota-corollaVersoII.JPG?alt=media&token=0b6cefc2-0baf-4797-aab7-674b85e8d619'));
+    this.carList.push( new Car(Guid.newGuid(), 'Volkswagen', 'Passat B1', '1973-1977', 45, 'Giorgetto Giugiaro', 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/volkswagen-passat-b1.jpg?alt=media&token=934750e8-b1c8-4f6b-87d2-23ea76328865'));
+    this.carList.push( new Car(Guid.newGuid(), 'Volkswagen', 'Passat B8', 'od 2014', 70, null, 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/volkswagen-passaatB8.jpg?alt=media&token=de2ad730-0b55-4b91-9be1-29fa8703c34e'));
+    this.carList.push( new Car(Guid.newGuid(), 'Toyota', 'Corolla Verso II', '2004-2009', 55, null, 'https://firebasestorage.googleapis.com/v0/b/cars-7bb35.appspot.com/o/toyota-corollaVersoII.JPG?alt=media&token=0b6cefc2-0baf-4797-aab7-674b85e8d619'));
   }
 
   ngOnInit() {
   }
 
   onAddCar() {
+    this.newCar.id = Guid.newGuid();
     this.carList.push( this.newCar );
   }
 
--- src/app/model/car.model.ts
+++ src/app/model/car.model.ts
@@ -1,4 +1,5 @@
 export class Car {
+    public id: string;
     public mark: string;
     public model: string;
     public productionYear: string;
@@ -6,12 +7,22 @@ export class Car {
     public designer: string;
     public image: string;
 
-    constructor(mark: string, model: string, productionYear: string, fuelTank: number, designer: string, image: string) {
+    constructor(id: string, mark: string, model: string, productionYear: string, fuelTank: number, designer: string, image: string) {
+        this.id = id;
         this.mark = mark;
         this.model = model;
         this.productionYear = productionYear;
         this.fuelTank = fuelTank;
         this.designer = designer;
         this.image = image;
+    }
+
+    guid() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 }
