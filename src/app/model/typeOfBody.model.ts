d1ca204de6bd60d8ece1637ea48ef3b0a419fe38
John <jasiek.ole@gmail.com>
2018-12-23 11:27:13 +0100

    Uzupelniono informacje o samochodach
    - dodano liczbe siedzen

--- src/app/model/typeOfBody.model.ts
+++ src/app/model/typeOfBody.model.ts
@@ -2,10 +2,12 @@ export class TypeOfBody {
     public carrier: string;
     public doors: number[];
     public name: string;
+    public seats: number[];
 
-    constructor(carrier: string, doors: number[], name: string) {
+    constructor(carrier: string, doors: number[], name: string, seats: number[]) {
         this.carrier = carrier;
         this.doors = doors;
         this.name = name;
+        this.seats = seats;
     }
 }
